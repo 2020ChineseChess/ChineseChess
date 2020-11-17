d2e301e1ce3251bb69f84df891be54b8f7857c39
CHUB <1292108841@qq.com>
2020-11-18 02:28:43 +0800

    #change the file name to standard #realize dp.display(gb)

---
+++ Displayer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChineseChess
+{
+    class Displayer
+    {
+      public  void DisplayBoard(GameBoard gb)
+        {
+            Console.WriteLine("  中国象棋欢迎您\n\n");
+
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(gb.board[i,j]);
+                }
+                Console.Write("\n");
+            }
+
+        }
+
+        void AskSelectPiece()
+        {
+            string flag;
+            char temp;
+
+            //gameBorad gb = gameborad;
+
+
+            //WriteAt("\nwhich piece you want to move? (e.g.: a2",0,21);
+            //WriteAt("", 0, 22);
+            flag = Console.ReadLine();
+
+            if (flag[0] >= 'a' && flag[0] <= 'i')
+            {
+                temp = flag[0];
+                string[] arr = flag.Split(temp);
+
+                //  WriteAt("Where do u want moving " + flag + " to?", 0,23);
+                flag = Console.ReadLine();
+
+                if ((flag[0] >= 'a' && flag[0] <= 'i') && (flag[1] >= 1 && flag[1] <= 9))
+                    //if (valid())
+
+                    Console.Clear();
+                //WriteAt("TEST", flag[0]+3,2*flag[1]);
+                //store game borad to class 'gb'
+                //could using int char[][] to store all element?
+                //change the value of [row][column] to change element.
+
+                //AskMovePiece(flag[1], flag[0], gb);
+
+            }
+            else
+            {
+                Console.WriteLine("illegal request, try again!");
+            }
+        }
+
+        void AskMovePiece(char row, char column, GameBoard gb)
+        {
+            gb.SwitchPlayer();
+        }
+    }
+
+}
--- GameBoard.cs
+++ GameBoard.cs
@@ -4,8 +4,15 @@ using System.Text;
 
 namespace ChineseChess
 {
+    
     class GameBoard
     {
+        public char[,] board;
+
+        public GameBoard()
+        {
+            chessboardBuilding();
+        }
         public void SwitchPlayer()
         {
             //if(Player == red)
@@ -25,6 +32,46 @@ namespace ChineseChess
         void CalculateValidMoves()
         {
 
+        }
+
+        void chessboardBuilding()
+        {
+            board = new char[12, 10];
+
+            //building the grid
+            for(int i = 0; i < 11; i++)
+            {
+                for(int j = 0; j< 9; j++)
+                {
+                    board[i,j] = '十';
+                }
+            }
+
+
+            //building the river
+            board[5, 0] = board[5, 1] = board[5, 4] = board[5, 7] = board[5, 8] = '一';
+            board[5, 2] = '楚';
+            board[5, 3] = '河';
+            board[5, 6] = '汉';
+            board[5, 6] = '界';
+
+            //building the RedChess
+            board[0, 0] = board[0, 8] = '車';
+            board[0, 1] = board[0, 7] = '马';
+            board[0, 2] = board[0, 6] = '相';
+            board[0, 3] = board[0, 5] = '仕';
+            board[0, 4] = '帅';
+            board[2, 1] = board[2, 7] = '炮';
+            board[3, 0] = board[3, 2] = board[3, 4] = board[3, 6] = board[3, 8] = '兵';
+
+            //building the BlackChess
+            board[10, 0] = board[10, 8] = '車';
+            board[10, 1] = board[10, 7] = '马';
+            board[10, 2] = board[10, 6] = '相';
+            board[10, 3] = board[10, 5] = '仕';
+            board[10, 4] = '将';
+            board[8, 1] = board[8, 7] = '炮';
+            board[7, 0] = board[7, 2] = board[7, 4] = board[7, 6] = board[7, 8] = '卒';
         }
     }
 }
